0c2bb2b47fe670db4824c9aec8a01b30b0386f3a
Tom Maffia <tmaffia@tbscg.com>
2024-08-04 16:30:48 -0400

    Fix guild tests after including context

--- guild_test.go
+++ guild_test.go
@@ -1,39 +1,46 @@
 package raiderio_test
 
-// func TestGetGuildRaidRankBySlug(t *testing.T) {
-// 	c := raiderio.NewClient()
+import (
+	"testing"
 
-// 	testCases := []struct {
-// 		region              *region.Region
-// 		realm               string
-// 		name                string
-// 		includeRandRankings bool
-// 		raidSlug            string
-// 		expectedRaidRank    int
-// 		expectedErrMsg      string
-// 	}{
-// 		{region: region.US, realm: "illidan", name: "warpath", raidSlug: "aberrus-the-shadowed-crucible", expectedRaidRank: 158, includeRandRankings: true},
-// 		{region: region.US, realm: "illidan", name: "warpath", raidSlug: "invalid raid slug", expectedErrMsg: "invalid raid", includeRandRankings: true},
-// 		{region: region.US, realm: "illidan", name: "warpath", raidSlug: "aberrus-the-shadowed-crucible",
-// 			expectedErrMsg: "guild raid rankings field missing from api response", includeRandRankings: false},
-// 	}
+	"github.com/tmaffia/raiderio"
+	"github.com/tmaffia/raiderio/region"
+)
 
-// 	for _, tc := range testCases {
-// 		profile, _ := c.GetGuild(&raiderio.GuildQuery{
-// 			Region:       tc.region,
-// 			Realm:        tc.realm,
-// 			Name:         tc.name,
-// 			RaidRankings: tc.includeRandRankings,
-// 		})
+func TestGetGuildRaidRankBySlug(t *testing.T) {
+	c := raiderio.NewClient()
 
-// 		rank, err := profile.GetGuildRaidRankBySlug(tc.raidSlug)
-// 		if err != nil && err.Error() != tc.expectedErrMsg {
-// 			t.Fatalf("expected error: %v, got: %v", tc.expectedErrMsg, err.Error())
-// 		}
+	testCases := []struct {
+		region              *region.Region
+		realm               string
+		name                string
+		includeRandRankings bool
+		raidSlug            string
+		expectedRaidRank    int
+		expectedErrMsg      string
+	}{
+		{region: region.US, realm: "illidan", name: "warpath", raidSlug: "aberrus-the-shadowed-crucible", expectedRaidRank: 158, includeRandRankings: true},
+		{region: region.US, realm: "illidan", name: "warpath", raidSlug: "invalid raid slug", expectedErrMsg: "invalid raid", includeRandRankings: true},
+		{region: region.US, realm: "illidan", name: "warpath", raidSlug: "aberrus-the-shadowed-crucible",
+			expectedErrMsg: "guild raid rankings field missing from api response", includeRandRankings: false},
+	}
 
-// 		if err == nil && rank.Mythic.World != tc.expectedRaidRank {
-// 			t.Fatalf("mythic guild ranking for raid: %v, got: %d, expected: %d",
-// 				rank.RaidSlug, rank.Mythic.World, tc.expectedRaidRank)
-// 		}
-// 	}
-// }
+	for _, tc := range testCases {
+		profile, _ := c.GetGuild(defaultCtx, &raiderio.GuildQuery{
+			Region:       tc.region,
+			Realm:        tc.realm,
+			Name:         tc.name,
+			RaidRankings: tc.includeRandRankings,
+		})
+
+		rank, err := profile.GetGuildRaidRankBySlug(tc.raidSlug)
+		if err != nil && err.Error() != tc.expectedErrMsg {
+			t.Fatalf("expected error: %v, got: %v", tc.expectedErrMsg, err.Error())
+		}
+
+		if err == nil && rank.Mythic.World != tc.expectedRaidRank {
+			t.Fatalf("mythic guild ranking for raid: %v, got: %d, expected: %d",
+				rank.RaidSlug, rank.Mythic.World, tc.expectedRaidRank)
+		}
+	}
+}
